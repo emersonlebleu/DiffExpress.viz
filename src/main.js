dd95719191ed5004f30843c49525123e014d4799
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-08-30 14:44:43 -0600

    Update dependencies
    Updated

--- src/main.js
+++ src/main.js
@@ -6,4 +6,15 @@ import App from './App.vue'
 // main.js
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
